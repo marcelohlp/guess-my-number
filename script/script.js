6dd5add00ee3ff89c7f96d237a79857b968ccb9b
marcelo paschoal <marcelo.hlp@outlook.com>
2025-06-09 16:07:48 -0300

    feat: logica de pontuacao e definicao de vencedor ou perdedor

--- script/script.js
+++ script/script.js
@@ -1,1 +1,32 @@
-'use strict';
+"use strict";
+
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
+let score = 20;
+
+document.querySelector(".number").textContent = secretNumber;
+
+document.querySelector(".check").addEventListener("click", () => {
+  const guess = Number(document.querySelector(".guess").value);
+
+  if (!guess) {
+    document.querySelector(".message").textContent = "No number!";
+  } else if (guess === secretNumber) {
+    document.querySelector(".message").textContent = "Correct number!";
+  } else if (guess > secretNumber) {
+    if (score > 1) {
+      document.querySelector(".message").textContent = "To high!";
+      score--;
+      document.querySelector(".score").textContent = score;
+    } else {
+      document.querySelector(".message").textContent = "You lost the game!";
+    }
+  } else if (guess < secretNumber) {
+    if (score > 1) {
+      document.querySelector(".message").textContent = "To low!";
+      score--;
+      document.querySelector(".score").textContent = score;
+    } else {
+      document.querySelector(".message").textContent = "You lost the game!";
+    }
+  }
+});
