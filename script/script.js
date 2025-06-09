a4743cc11e370d2f0ffb65179a472adc360bff52
marcelo paschoal <marcelo.hlp@outlook.com>
2025-06-09 18:54:16 -0300

    feat: logica de armazenar a maior pontuacao e logica de iniciar uma nova partida

--- script/script.js
+++ script/script.js
@@ -1,15 +1,18 @@
 "use strict";
 
-const secretNumber = Math.trunc(Math.random() * 20) + 1;
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
 let gameScore = 20;
+let gameHighscore = 0;
 let finished = false;
 
 const body = document.querySelector("body");
 const message = document.querySelector(".message");
 const number = document.querySelector(".number");
 const score = document.querySelector(".score");
+const highscore = document.querySelector(".highscore");
 const inputValue = document.querySelector(".guess");
 const checkButton = document.querySelector(".check");
+const againButton = document.querySelector(".again");
 
 checkButton.addEventListener("click", () => {
   const guess = Number(inputValue.value);
@@ -17,8 +20,12 @@ checkButton.addEventListener("click", () => {
   if (!finished) {
     if (!guess) {
       message.textContent = "No number!";
+    } else if (guess < 1 || guess > 20) {
+      message.textContent = "Only numbers from 1 to 20!";
+      inputValue.value = "";
     } else if (guess === secretNumber) {
       winTheGame();
+      defineHighScore();
     } else if (guess !== secretNumber) {
       if (gameScore > 1) {
         message.textContent = guess > secretNumber ? "Too high!" : "Too low!";
@@ -52,6 +59,13 @@ const decreaseTheScore = () => {
   score.textContent = gameScore;
 };
 
+const defineHighScore = () => {
+  if (gameScore > gameHighscore) {
+    gameHighscore = gameScore;
+    highscore.textContent = gameHighscore;
+  }
+};
+
 const changeBackgroundColor = (color) => {
   body.style.backgroundColor = color;
 };
@@ -61,7 +75,29 @@ const showSecretNumber = () => {
   number.style.width = "30rem";
 };
 
+const hideSecretNumber = () => {
+  number.textContent = "?";
+  number.style.width = "15rem";
+};
+
+const enableTheGame = () => {
+  checkButton.disabled = false;
+  inputValue.disabled = false;
+};
+
 const disableTheGame = () => {
   checkButton.disabled = true;
   inputValue.disabled = true;
 };
+
+againButton.addEventListener("click", () => {
+  hideSecretNumber();
+  changeBackgroundColor("#222");
+  enableTheGame();
+  secretNumber = Math.trunc(Math.random() * 20) + 1;
+  gameScore = 20;
+  finished = false;
+  message.textContent = "Start guessing...";
+  score.textContent = "20";
+  inputValue.value = "";
+});
